7c6a75ea89e994a3af3b7d7e8439526e5c52f4eb
yunzimo <yunzimolikeyou@outlook.com>
2018-11-25 15:17:49 +0800

    Site updated: 2018-11-25 15:17:49

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/TEP.ico");
-         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
+         document.title = '(つ ェ ⊂)我藏好了哦~';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/favicon.ico");
-         document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
+         document.title = '(*゜ロ゜)ノ被发现了~' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
